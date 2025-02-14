5d4d469797e4a40c0f948f66c7853ed6ed19788f
RishabJituri <rishab.jituri@gmail.com>
2025-02-14 17:14:48 -0500

    no GPUs on main partition

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import torch
 import torch.nn as nn
 from accelerate import Accelerator
 import argparse
+from models.unet_utils import *
 
+if torch.cuda.is_available(): print("working") 
+else: print("not working")
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# model = UNet_Encoder_Layer()
 
+# parser = argparse.ArgumentParser()
+# parser.add_argument("config file")
+# args = parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("config file")
-args = parser.parse_args()
 
