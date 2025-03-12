5c04e112692a738f4184ab9f83285243fbb68964
RishabJituri <rishab.jituri@gmail.com>
2025-03-11 21:42:39 -0400

    more work with gnn

--- models/neighborhood_gnn.py
+++ models/neighborhood_gnn.py
@@ -2,13 +2,17 @@ import torch
 import torch.nn as nn
 from torch_geometric.nn import ChebConv
 from torch_geometric.nn import aggr
+from torch_geometric.nn import models
+
 
 
 class default_gnn(nn.Module):
     
     def __init__(self, in_channels:int, 
                  conv_out_channels:int,
+                 edges:int,
                  k_filter_size: int,
+                 dense_layer_in:int,
                  dense_layer_out: int,
                  num_embeddings:int,
                  embedding_dims:int
@@ -23,36 +27,54 @@ class default_gnn(nn.Module):
             )
 
         self.dense_layer = nn.Linear(
-            in_features = conv_out_channels, 
+            in_features = dense_layer_in, 
             out_features = dense_layer_out
             )
         
-        self.final_node_embedding = nn.Linear(
-            in_features=num_embeddings,
-            out_features=embedding_dims
-        )
+        # self.final_node_embedding = nn.Linear(
+        #     in_features=num_embeddings,
+        #     out_features=embedding_dims
+        # )
+        
+        self.final_node_embedding = 
+        
+        self.edges = edges
         
     def forward(self,x):
-        x = self.gcl(x,torch.randint(low=0,high=1,size=(2,2)))
-        x = aggr.MeanAggregation()(x)
-        x = self.dense_layer(x)    
+        print(f"x  {x.size()}")
+        x = self.gcl(x,torch.randint(low=0,high=1,size=(2,self.edges)))
+        print(f"x  {x.size()}")
+        x = aggr.MeanAggregation()(x,dim=-1)
+        print(f"x  {x.size()}")
+        x = x.squeeze()
+        print(f"x  {x.size()}")
+        x = self.dense_layer(x) 
+        print(f"x  {x.size()}")   
+        x = x.T
+        print(f"x  {x.size()}")   
         x = self.final_node_embedding(x)
-        print(f"x final embedding {x}")
+        print(f"x final embedding {x.size()}")
         return x
 
-def test():
+
+#srun --time=02:00:00 python3 ./models/neighborhood_gnn.py
+
+def test(n:int,m:int):
     
-    x = torch.randn(4,4)
+    x = torch.randint(0,1,(n,n,3)).to(torch.float)
+    
     
     model = default_gnn(
-        in_channels=4,
-        conv_out_channels=1,
+        in_channels=-1,
+        conv_out_channels=3,
+        edges=100,
         k_filter_size=1,
-        dense_layer_out = 1,
-        num_embeddings=1,
-        embedding_dims=1
+        dense_layer_in = n,
+        dense_layer_out = 10,
+        num_embeddings=n,
+        embedding_dims=m
         )
     
-    return model(x)
+    return model(x).size()
 
-test()
+test(10,60)
