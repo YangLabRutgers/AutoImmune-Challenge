9ce4de818f1da9e7a5d9541c5a72f0b5d7d90b3e
RishabJituri <rishab.jituri@gmail.com>
2025-03-11 22:36:24 -0400

    documentation added

--- models/neighborhood_gnn.py
+++ models/neighborhood_gnn.py
@@ -5,10 +5,27 @@ from torch_geometric.nn import aggr
 from torch_geometric.nn import models
 
 """
-Default GNN class comprised of Chebyshev convolutional 
+Default GNN class comprised of  v 
 layer, mean aggregation across the 3 channels, one 
 dense layer and an embedding layer.
 
+n = nodes
+
+e = edges
+
+x in R^{n X n X 3}
+edge_list in R^{2 X e}
+
+ChebConv: R^{n X n X 3} X R^{2 X e} -> R^{n X n X 3}
+
+Aggregate Layer: R^{n X n X 3} -> R^{n X n}
+
+Dense Layer: R^{n X n} -> R^{n X n}
+
+Embedding Layer: R^{n X n} -> R^{n X m} 
+
+such that m is the dimension of each embedding, 
+resulting in n nodes, each with m dims
 
 """
 class default_gnn(nn.Module):
@@ -51,7 +68,7 @@ class default_gnn(nn.Module):
         
         x = aggr.MeanAggregation()(x,dim=-1)
         
-        x = x.squeeze()
+        x = x.squeeze() #[n X n X 1] -> [n X n]
         
         x = self.dense_layer(x) 
         
@@ -64,16 +81,16 @@ class default_gnn(nn.Module):
 
 #srun --time=02:00:00 python3 ./models/neighborhood_gnn.py
 
-def test(n:int,m:int):
+# def test(n:int,m:int):
     
-    x = torch.randint(0,1,(n,n,3)).to(torch.float)
+#     x = torch.randint(0,1,(n,n,3)).to(torch.float)
     
-    edge_index = torch.randint(0,1,[2,n*n])
+#     edge_index = torch.randint(0,1,[2,n*n])
     
-    model = default_gnn(
-        nodes = n, edges = m
-        )
+#     model = default_gnn(
+#         nodes = n, edges = m
+#         )
     
-    return model(x,edge_index).size()
+#     return model(x,edge_index).size()
 
-test(10,60)
+# test(10,60)
